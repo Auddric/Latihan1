8f90854fa8272a4653e91200cb692991449c3c38
Auddric <audricshidqi08@gmail.com>
2025-05-21 09:31:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,4 @@ st.title("WUJUD ASLI PENAMPAKAN HANTU TAHURA")
 st.write(
     "berikut bukti foto PENAMPAKAN"
 )
-st.image("IMG-20250514-WA0001.jpg", widht=200)
-st.image("IMG-20250514-WA0001.jpg", width=200)
-st.image("IMG-20250514-WA0001.jpg", width=200)
+st.image(IMG-20250514-WA0001.jpg", width=200)
