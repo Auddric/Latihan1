85599dc069693d1a9bc753b57dc7be2daf00a0fc
Auddric <audricshidqi08@gmail.com>
2025-05-21 09:27:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.title("selamat datang di pelajaran informatika ")
+st.title("WUJUD ASLI PENAMPAKAN HANTU TAHURA")
 st.write(
-    "Belajar coding bersama Pa Hendri"
+    "berikut bukti foto PENAMPAKAN"
 )
 st.image("IMG-20250514-WA0001.jpg", widht=200)
+st.image("IMG-20250514-WA0001.jpg", width=200)
 st.image("IMG-20250514-WA0001.jpg", width=200)
